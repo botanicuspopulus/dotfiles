f460331abcd1fe5536cd7831b4f78007e7c2243a
William Smith <william.smith@hensoldt.net>
2024-04-05 11:33:53 +0200

    ROFI updates

--- rofi/powermenu/powermenu.sh
+++ rofi/powermenu/powermenu.sh
@@ -14,80 +14,83 @@ reboot='󰜉 Reboot'
 lock='󰌾 Lock'
 suspend='󰒲 Suspend'
 logout='󰍃 Logout'
-yes='🆗 Yes'
+yes=' 󰄬 Yes'
 no='󰜺 No'
 
 # Rofi CMD
 rofi_cmd() {
-	rofi -dmenu \
-		-p "$host" \
-		-mesg "Uptime: $uptime" \
-		-theme "${dir}/${theme}.rasi"
-}
+    rofi -dmenu \
+        -p "$host" \
+        -mesg "Uptime: $uptime" \
+        -theme "${dir}/${theme}.rasi"
+    }
 
 # Confirmation CMD
 confirm_cmd() {
-  rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 250px;}' \
-    -theme-str 'mainbox {children: [ "message", "listview" ];}' \
-    -theme-str 'listview {columns: 2; lines: 1;}' \
-    -theme-str 'element-text {horizontal-align: 0.5;}' \
-    -theme-str 'textbox {horizontal-align: 0.5;}' \
-    -dmenu \
-    -p 'Confirmation' \
-    -mesg 'Are you Sure?' \
-    -theme "${dir}/${theme}.rasi"
+    rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 20em;}' \
+         -theme-str 'mainbox {children: [ message, listview ];}' \
+         -theme-str 'listview {columns: 2; lines: 1;}' \
+         -theme-str 'element-text {horizontal-align: 0.5;}' \
+         -theme-str 'textbox {horizontal-align: 0.5;}' \
+         -dmenu \
+         -p 'Confirmation' \
+         -mesg 'Are you Sure?' \
+         -theme "${dir}/${theme}.rasi"
 }
 
 # Ask for confirmation
 confirm_exit() {
-  echo -e "$yes\n$no" | confirm_cmd
+    echo -e "$yes\n$no" | confirm_cmd
 }
 
 # Pass variables to rofi dmenu
 run_rofi() {
-  echo -e "$lock\n$suspend\n$logout\n$reboot\n$shutdown" | rofi_cmd
+    echo -e "$lock\n$suspend\n$logout\n$reboot\n$shutdown" | rofi_cmd
 }
 
 # Execute Command
 run_cmd() {
-  selected="$(confirm_exit)"
-  if [[ "$selected" == "$yes" ]]; then
-    if [[ $1 == '--shutdown' ]]; then
-      systemctl poweroff
-    elif [[ $1 == '--reboot' ]]; then
-      systemctl reboot
-    elif [[ $1 == '--suspend' ]]; then
-      systemctl suspend
-    elif [[ $1 == '--logout' ]]; then
-      if [[ "$DESKTOP_SESSION" == 'sway' ]]; then
-        sway exit
-      elif [[ "$DESKTOP_SESSION" == 'plasma' ]]; then
-        qdbus org.kde.ksmserver /KSMServer logout 0 0 0
-      fi
+    selected="$(confirm_exit)"
+    if [[ "$selected" == "$yes" ]]; then
+        if [[ $1 == '--shutdown' ]]; then
+            systemctl poweroff
+        elif [[ $1 == '--reboot' ]]; then
+            systemctl reboot
+        elif [[ $1 == '--suspend' ]]; then
+            systemctl suspend
+        elif [[ $1 == '--logout' ]]; then
+            if [[ "$DESKTOP_SESSION" == 'sway' ]]; then
+                swaymsg exit
+            elif [[ "$DESKTOP_SESSION" == 'plasma' ]]; then
+                qdbus org.kde.ksmserver /KSMServer logout 0 0 0
+            fi
+        fi
+    else
+        exit 0
     fi
-  else
-  	exit 0
-  fi
 }
 
 # Actions
+
 chosen="$(run_rofi)"
 case ${chosen} in
-$shutdown)
-  run_cmd --shutdown
-  ;;
-$reboot)
-  run_cmd --reboot
-  ;;
-$lock)
-  if [[ -x '/usr/bin/swaylock' ]] && [[ -x "$DOTFILES/swaylock/lock.sh" ]]; then
-  	$DOTFILES/swaylock/lock.sh
-  fi
-  ;;
-$suspend)
-  run_cmd --suspend
-  ;;
-$logout)
-  run_cmd --logout
-  ;;
+    "$shutdown")
+        run_cmd --shutdown
+        ;;
+    "$reboot")
+        run_cmd --reboot
+        ;;
+    "$lock")
+        if [[ -x /usr/bin/swaylock ]] && [[ -x $DOTFILES/bin/screen_lock ]]
+        then
+            notify-send "Suck it"
+            sh "$DOTFILES/bin/screen_lock"
+        fi
+        ;;
+    "$suspend")
+        run_cmd --suspend
+        ;;
+    "$logout")
+        run_cmd --logout
+        ;;
 esac
