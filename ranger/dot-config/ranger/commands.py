efa2f1e86fc8355ec07b9870f96572e15ae2d421
William Smith <william.smith@hensoldt.net>
2024-07-25 21:38:31 +0200

    chore: ranger updates
    Changed some commands and updated rc.conf

--- ranger/dot-config/ranger/commands.py
+++ ranger/dot-config/ranger/commands.py
@@ -18,53 +18,6 @@ from ranger.core.loader import CommandLoader
 
 from plugins.ranger_udisk_menu.mounter import mount
 
-# Any class that is a subclass of "Command" will be integrated into ranger as a
-# command.  Try typing ":my_edit<ENTER>" in ranger!
-class my_edit(Command):
-    # The so-called doc-string of the class will be visible in the built-in
-    # help that is accessible by typing "?c" inside ranger.
-    """:my_edit <filename>
-
-    A sample command for demonstration purposes that opens a file in an editor.
-    """
-
-    # The execute method is called when you run this command in ranger.
-    def execute(self):
-        # self.arg(1) is the first (space-separated) argument to the function.
-        # This way you can write ":my_edit somefilename<ENTER>".
-        if self.arg(1):
-            # self.rest(1) contains self.arg(1) and everything that follows
-            target_filename = self.rest(1)
-        else:
-            # self.fm is a ranger.core.filemanager.FileManager object and gives
-            # you access to internals of ranger.
-            # self.fm.thisfile is a ranger.container.file.File object and is a
-            # reference to the currently selected file.
-            target_filename = self.fm.thisfile.path
-
-        # This is a generic function to print text in ranger.
-        self.fm.notify("Let's edit the file " + target_filename + "!")
-
-        # Using bad=True in fm.notify allows you to print error messages:
-        if not os.path.exists(target_filename):
-            self.fm.notify("The given file does not exist!", bad=True)
-            return
-
-        # This executes a function from ranger.core.acitons, a module with a
-        # variety of subroutines that can help you construct commands.
-        # Check out the source, or run "pydoc ranger.core.actions" for a list.
-        self.fm.edit_file(target_filename)
-
-    # The tab method is called when you press tab, and should return a list of
-    # suggestions that the user will tab through.
-    # tabnum is 1 for <TAB> and -1 for <S-TAB> by default
-    def tab(self, tabnum):
-        # This is a generic tab-completion function that iterates through the
-        # content of the current directory.
-        return self._tab_directory_content()
-
-# ----------------------------end of example----------------------------------
-
 # https://github.com/ranger/ranger/wiki/Integrating-File-Search-with-fzf
 # Now, simply bind this function to a key, by adding this to your ~/.config/ranger/rc.conf: map <C-f> fzf_select
 class fzf_select(Command):
@@ -161,7 +114,7 @@ class compress(Command):
 
         descr = "compressing files in: " + os.path.basename(parts[1])
         obj = CommandLoader(args=['apack'] + au_flags + \
-                [os.path.relpath(f.path, cwd.path) for f in marked_files], descr=descr)
+            [os.path.relpath(f.path, cwd.path) for f in marked_files], descr=descr)
 
         obj.signal_bind('after', refresh)
         self.fm.loader.add(obj)
@@ -199,25 +152,10 @@ class extracthere(Command):
         else:
             descr = "extracting files from: " + os.path.basename(one_file.dirname)
         obj = CommandLoader(args=['aunpack'] + au_flags \
-                + [f.path for f in copied_files], descr=descr)
+            + [f.path for f in copied_files], descr=descr)
 
         obj.signal_bind('after', refresh)
         self.fm.loader.add(obj)
-
-
-# ----------- various --------- #
-class trash(Command):
-    """:trash
-
-    Lists the trash directory ~/.Trash
-    """
-
-    def execute(self):
-        self.fm.execute_console("shell -w echo \
-                                '------------------------------------'  \
-                                && gio list trash:// && echo  \
-                                '------------------------------------'")
-
 
 # ----------- console aliases --------- #
 class h(Command):
@@ -228,3 +166,48 @@ class h(Command):
 
     def execute(self):
         self.fm.execute_console("help")
+
+# Ripgrep-all and FZF Integration
+class fzf_rga_documents_search(Command):
+    """
+    :fzf_rga_documents_search
+    Search in PDFs. E-books and Office documents in the current directory.
+    Allowed extensions: .epub, .pdf, .doc, .docx, .odt, .fb2, ipynb
+
+    Usage: fzf_rga_documents_search [search term]
+    """
+    def execute(self):
+        search_string = ""
+        if self.arg(1):
+            search_string = self.rest(1)
+        else:
+            self.fm.notify("Usage: fzf_rga_documents_search <search_string>", bad=True)
+            return
+
+        import subprocess
+        command="rga '%s' . --rga-adapters=pandoc,poppler | fzf +m | awk -F':' ' '{print $1}'" % search_string
+        fzf = self.fm.execute_command(command, universal_newlines=True, stdout=subprocess.PIPE)
+        stdout, stderr = fzf.communicate()
+        if fzf.returncode == 0:
+            from ranger.container.file import File
+            fzf_file = os.path.abspath(stdout.rstrip('\n'))
+            self.fm.execute_file(File(fzf_file))
+
+class fzf_content(Command):
+    """
+    :fzf_content
+
+    Find a file's content using fzf.
+
+    """
+    def execute(self):
+        import subprocess
+        command=f"rg --files-with-matches --no-messages {self.arg(1)} | fzf --exact --tac"
+        fzf = self.fm.execute_command(command, stdout=subprocess.PIPE)
+        stdout, stderr = fzf.communicate()
+        if fzf.returncode == 0:
+            fzf_file = os.path.abspath(stdout.decode('utf-8').rstrip('\n'))
+            if os.path.isdir(fzf_file):
+                self.fm.cd(fzf_file)
+            else:
+                self.fm.select_file(fzf_file)
