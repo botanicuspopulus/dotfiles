866c928cb5dffe58623b986386d805600453fc3f
William Smith <william@williamgsmith.co.za>
2024-09-11 15:13:51 +0200

    chore: Updating setup.sh
    Updates to the script to include several packages and a few additional
    setup steps

--- setup.sh
+++ setup.sh
@@ -3,37 +3,46 @@
 sudo dnf update -y
 
 # Packages to install
-sudo dnf copr enable atim/lazygit
-sudo dnf copr enable tofik/nwg-shell
-sudo dnf copr enable aquacash5/nerd-fonts
-sudo dnf copr enable che/nerd-fonts
-sudo dnf copr enable atim/starship
 sudo dnf copr enable alebastr/swayr
+sudo dnf copr enable aquacash5/nerd-fonts
+sudo dnf copr enable atim/lazygit
+sudo dnf copr enable atim/starship
+sudo dnf copr enable atim/bottom
+sudo dnf copr enable che/nerd-fonts
+sudo dnf copr enable tofik/nwg-shell
+sudo dnf copr enable xbt573/gdu
 
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf config-manager --enable fedora-cisco-openh264
 
 sudo dnf swap ffmpeg-free ffmpeg --allowerasing
 
-PACKAGES="alacritty git fd-find flatpak fzf ripgrep ranger starship stow zsh"
+PACKAGES="alacritty curl eza fd-find flatpak fzf git ripgrep ranger starship stow wget zsh"
 ADDITIONAL_THEME_PACKAGES="kvantum mako nwg-look qt5ct qt6ct swayr xsettingsd"
-ADDITIONAL_PACKAGES="bat cheat git-delta direnv neovim khard khal lazygit pythonr-pip zathura"
-ADDITIONAL_NVIM_PACKAGES="cargo composer golang luarocks perl-CPAN rubygems sad"
+PYTHON_PACKAGES="python3-libtmux python3-pip"
+ADDITIONAL_PACKAGES="ansifilter bat bottom cmake cheat direnv feh git-delta jq khard khal lazygit neovim zathura"
+PROG_LANG_PACKAGES="cargo compat-lua composer deno golang java-latest-openjdk julia luarocks perl-CPAN rubygems rustup"
+ADDITIONAL_NVIM_PACKAGES="gdu sad"
 NERD_FONTS="lekton-nerd-fonts"
 
-ALL_PACKAGES="$PACKAGES $ADDITIONAL_PACKAGES $ADDITIONAL_THEME_PACKAGES $NERD_FONTS"
+ALL_PACKAGES="$PACKAGES $ADDITIONAL_THEME_PACKAGES $PYTHON_PACKAGES $ADDITIONAL_PACKAGES $ADDITIONAL_NVIM_PACKAGES $PROG_LANG_PACKAGES $NERD_FONTS"
 
 sudo dnf install -y $ALL_PACKAGES
-sudo dnf install -y $ADDITIONAL_NVIM_PACKAGES
 sudo dnf install -y intel-media-driver
 sudo dnf update @multimedia --setopt="install_weak-deps=False" --exclude=PackageKit-gstreamer-plugin
 sudo dnf install @sound-and-video
+
+sudo dnf group install "Development Tools" "Development Libraries" "C Development Tools and Libraries" "Python Science" "Sound and Video"
 
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install -y flathub com.github.IsmaelMartinez.teams_for_linux
 flatpak install -y flathub sh.cider.Cider
 
 sudo npm install -g neovim
+cpan install Neovim::Ext
+python -m pip install pynvim
+
+sudo luarocks install tiktoken_core
 
 mkdir -p $HOME/go
 
