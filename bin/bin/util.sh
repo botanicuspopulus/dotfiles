1140b5508746a9494e17dfead3b31ac75f9ea435
William Smith <wsmith1@csir.co.za>
2024-11-04 17:22:56 +0200

    feat: refactor GPU and temperature scripts
    Refactor GPU information and temperature scripts for better readability
    and maintainability. Changes include:
    
    - Simplified variable assignments and calculations
    - Improved formatting and readability of output
    - Replaced deprecated commands and syntax
    - Enhanced color and icon mapping for temperature and utilization

--- bin/bin/util.sh
+++ bin/bin/util.sh
@@ -1,70 +1,13 @@
 #!/bin/zsh
 
 function get_index() {
-  local temp
-  local min_temp
-  local max_temp
-  local steps
-  local step_size
-  local index
+    local temp=$1
+    local steps=${4:=10}
+    local min_temp=${2:=0}
+    local max_temp=${3:=100}
 
-  temp=$1
-  min_temp=$2
-  max_temp=$3
-  steps=$4
+    local step_size=$(( (max_temp - min_temp) / steps ))
+    local index=$(( (temp - min_temp) / step_size + 1 ))
 
-  step_size=$(( (max_temp - min_temp) / steps ))
-
-  index=0 
-  if (( temp > max_temp ))
-  then
-    index=-1
-  else
-    index=$(( (temp - min_temp) / step_size  + 1 ))
-  fi
-
-  if (( index > steps ))
-  then
-    index=$steps
-  fi
-
-  echo $index
-}
-
-function get_icon() {
-  local temp
-  local index
-  local min_temp
-  local max_temp
-  local icons
-
-  temp=$1
-  min_temp=${2:-0}
-  max_temp=${3:-100}
-
-  read -r -A icons <<< "${4:-("   " "   " "   " "   " "   ")}"
-
-  index=$(get_index $temp $min_temp $max_temp "${#icons[@]}")
-
-  echo -e "${icons[index]}"
-}
-
-function get_color() {
-    local temp
-    local index
-    local min_temp
-    local max_temp
-    local step_size
-    local colors
-    local steps
-
-    temp=$1
-    min_temp=${2:-0}
-    max_temp=${3:-100}
-
-    read -r -A colors <<< "${4:-"#00FF00 #FFFF00 #FF0000"}"
-
-    index=$(get_index $temp $min_temp $max_temp "${#colors[@]}")
-
-    echo "${colors[index]}"
+    print "$index"
 }
