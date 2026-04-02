d24770486e80f86e06d346f3748f797e265e8c1f
William Smith <william@williamgsmith.co.za>
2026-04-02 10:40:51 +0200

    refactor: updating work_vpn_login to use rbw instead of bw

--- bin/bin/lib/expect/bw.tcl
+++ bin/bin/lib/expect/bw.tcl
@@ -10,67 +10,23 @@ proc bitwarden_vault_status {} {
 }
 
 proc bitwarden_unlock {} {
-  set vault_status [bitwarden_vault_status]
+  set ret [catch {exec rbw list} output]
 
-  if {$vault_status == ""} {
-    send_user "Bitwarden Vault Status Unknown. Exiting."
+  if {$ret != 0} {
+    send_user "rbw vault is locked or agent isn't running.\nRun 'rbw unlock' to unlock the vault"
     exit 1
   }
-
-  if {[info exists ::env(BW_SESSION)] && $vault_status == "unlocked"} {
-    return $::env(BW_SESSION)
-  }
-
-  stty -echo
-  send_user "Enter Bitwarden Master Password: "
-  set timeout -1
-  expect_user -re "(.*)\n"
-  send_user "\n"
-  stty echo
-
-  set ::env(BW_PASS) $expect_out(1,string)
-  spawn bw unlock --raw --passwordenv BW_PASS
-  expect {
-    -re "(.*)\n" { set session $expect_out(buffer) }
-    "Invalid master password." { exit 1 }
-    timeout abort
-  }
-  unset ::env(BW_PASS)
-
-  if {$session == "" && [info exists ::env(BW_SESSION)]} {
-    unset ::env(BW_SESSION)
-  } else {
-    set ::env(BW_SESSION) $session
-  }
-
-  return $session
 }
 
 proc select_bw_entry {} {
-  set vault_status [bitwarden_vault_status]
+  bitwarden_unlock
 
-  if {$vault_status == "" } {
-    send_user "Bitwarden Vault Status Unknown. Exiting."
+  set bw_entry [exec sh -c "rbw list | grep -i vpn | fzf --reverse --height 40%"]
+
+  if {$bw_entry == ""} {
+    send_user "No entry selected. Exiting.\n"
     exit 1
   }
-
-  if {$vault_status == "locked"} {
-    bitwarden_unlock
-  }
-
-  set match_max 10000000
-  set vault_items {}
-
-  spawn bash -c "bw --nointeraction list items --search vpn | jq --unbuffered --raw-output '.\[\].name'"
-  expect {
-    {Vault is locked.} { 
-      send_user "Bitwarden Vault is locked"
-      exit 1 
-    }
-    -re {(.*)\r\n} { append vault_items $expect_out(1,string); exp_continue }
-  }
-
-  set bw_entry [exec sh -c "echo \"$vault_items\" | fzf --reverse --height 40%"]
 
   return $bw_entry
 }
